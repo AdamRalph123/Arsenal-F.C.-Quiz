ceda429b0b3c2e0bb3def12a07d11c2cd38799b6
AdamRalph123 <adambyrne16@gmail.com>
2022-11-19 14:10:04 +0000

    Created an end.html and an end.js and got my code working and fixed and error on the highscores page and also styled it in css

---
+++ assets/js/highscores.js
@@ -0,0 +1,9 @@
+const username = document.getElementById('username');
+const saveButton = document.getElementById('saveScoreBtn');
+const highScoresList = document.querySelector('#highScoresList');
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
+highScoresList.innerHTML =
+highScores.map(score => {
+    return `<li class="high-score">${score.name} - ${score.score}</li>`
+}).join("")
